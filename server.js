997a1b5f6c2c51a1a97d40a565dcfd36011624db
Ubuntu <ubuntu@ip-172-31-42-7.ap-southeast-1.compute.internal>
2019-11-19 06:47:41 +0000

    Add hello-world server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello-Wordl');
+});
+
+app.listen(8080, () => {
+	console.log('App is running on port 8080');
+});
